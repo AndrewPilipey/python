96f0254d114b6cedaf33a754809a049df630c97b
https://github.com/AndrewPilipey <andrewpilipeyone@gmail.com>
2022-11-25 22:51:02 +0300

    M

---
+++ 6lessiion.py
@@ -0,0 +1,11 @@
+weather = "Sunny"
+time = "Noon"
+
+if weather == "Rain" and time == "Noon":
+    print("Catch a rainbow")
+
+if weather == "Sunny" and (time =="Morning" or time == "Noon"):
+    print("Welcome to hell!")
+
+if not weather == "Humid":
+    print("it's definitely dry")
