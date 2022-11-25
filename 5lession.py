fb6e1f88c4b267456ee5f55664759961e064a09e
https://github.com/AndrewPilipey <andrewpilipeyone@gmail.com>
2022-11-25 22:07:42 +0300

    sintax

---
+++ 5lession.py
@@ -0,0 +1,18 @@
+season = "winter"
+time = "Night"
+
+print("App is starting")
+
+if season == "winter":
+    print("it's cold now")
+    if time == "Night":
+        print("Are you fool? Sleep.")
+    if time == "Day":
+        print("Well. And what do you want?")
+
+if season == "summer":
+    print("It's probably hot.")
+    if time == "Night":
+        print("You are going to dancing?")
+    if time == "Day":
+        print("I will go to vacation this summer.")
