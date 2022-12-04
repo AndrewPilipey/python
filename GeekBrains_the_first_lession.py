10c4a75d9371d6563e96dd9b48c475b18242af7c
https://github.com/AndrewPilipey <andrewpilipeyone@gmail.com>
2022-12-04 23:48:24 +0300

    +GeekBrains_first_lession

---
+++ GeekBrains_the_first_lession.py
@@ -0,0 +1,12 @@
+print("Hello, \n\"world!\"")
+print(2 + 2)
+
+test = 10
+
+print("I'm " + str(test) + " years old")
+name = input("What is your name? : ")
+
+a = input("Enter a number one, " + name + " : ")
+b = input("Enter a number two, " + name + " : ")
+
+print((int(a) + int(b)) * 3)
